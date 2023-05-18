15c5c351030618fe1d5434ce89b762fb4c4108b3
lee-cha-dev <leecharlesdev@gmail.com>
2023-05-18 06:07:24 -0500

    Added in html for resume.js. Complete css and media queries. Scales for most sizes, went for a overview, rather than readibility for all viewers. Assumed user will scroll into their preference for reading documents or download res.

--- js/pages/Resume.js
+++ js/pages/Resume.js
@@ -10,6 +10,17 @@ const Resume = () => {
             "   <h1>Resume</h1>" +
             "</div>"
         ) +
+        AnimatedSection(
+            "" +
+            "<div class='resume-page-div'>" +
+            "   <object data='./media/docs/resume_2023.pdf#view=FitH' type='application/pdf'>" +
+            "       <p>" +
+            "           Unable to display Resume/PDF file. " +
+            "           <a href='./media/docs/resume_2023.pdf'>Download</a> Instead" +
+            "       </p>" +
+            "   </object>" +
+            "</div>"
+        ) +
         "</div>";
 
     return (resumeElements);
